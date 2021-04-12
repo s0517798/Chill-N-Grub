f21234b82202f015bb334d299aa751bd116605f3
PaitoneMyla <81176105+PaitoneMyla@users.noreply.github.com>
2021-04-12 08:07:08 +0800

    Add files via upload

---
+++ Customer/menu.php
@@ -0,0 +1,85 @@
+<?php include('db_conn.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CUSTOMER</title>
+<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<?php include('heading.php')?>
+
+
+<?php include('nav.php'); ?>
+<div class=""></div>
+<div class="container">
+	<h1 class="page-header text-center">MENU</h1>
+	
+	
+	
+
+	<form method="POST" action="buy.php">
+		
+		<table class="table ">
+			<thead>
+				<th class="text-center"><input type="checkbox" id="checkAll"></th>
+				<th>Category</th>
+				<th>Image</th>
+				<th>Product Name</th>
+				<th>Price</th>
+				<th>Quantity</th>
+			</thead>
+			<tbody>
+				<?php
+					$where = "";
+					if(isset($_GET['category']))
+					{
+						$catid=$_GET['category'];
+						$where = " WHERE product.categoryid = $catid";
+					}
+                
+					$sql="SELECT * FROM product left JOIN category on category.cat_id=product.cat_id
+							LEFT JOIN pricing on product.prod_id = pricing.prod_id order by product.cat_id
+							asc,prod_name asc;";
+					$query=$conn->query($sql);
+					$iterate=0;
+					while($row=$query->fetch_array()){
+						?>
+						<tr>
+						
+						<td class="text-center"><input type="checkbox" value="<?php echo $row['prod_id']; ?>||<?php echo $iterate; ?>" name="prod_id[]" style=""></td>
+							<td><?php echo $row['cat_desc']; ?></td>
+							<td><img src="<?php echo $row['prod_img']?>" height="100px" width="100px"></td>
+							<td><?php echo $row['prod_name']; ?></td>
+							<td class="text-left">&#8369; <?php echo number_format($row['price'], 2); ?></td>
+							<td><input type="text" class="form-control" name="qty_<?php echo $iterate; ?>"></td>
+						</tr>
+						<?php
+						$iterate++;
+					}
+				?>
+			</tbody>
+		</table>
+		
+		<div class="row">
+			<div class="col-md-3">
+				<input type="text" name="tblnum" class="form-control" placeholder="Table Number" required>
+			</div>
+			<div class="col-md-2" style="margin-left:5px;">
+				<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Order</button>
+			</div>
+		</div>
+	</form>
+</div>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#checkAll").click(function(){
+	    	$('input:checkbox').not(this).prop('checked', this.checked);
+		});
+	});
+</script>
+
+
+</body>
+</html>
---
+++ Customer/sales.php
@@ -0,0 +1,48 @@
+<?php include('db_conn.php'); ?>
+<?php include('heading.php'); ?>
+<?php include('nav.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sales</title>
+<link rel="stylesheet" type="text/css" href="sales.css">
+</head>
+
+
+
+<body>
+<div class="container">
+
+	<h1 class="page-header text-center">ORDERS</h1>
+	<table class="table">
+		<thead>
+			<th>Date</th>
+			<th>Table Name</th>
+			<th>Total Sales</th>
+			<th>Details</th>
+		</thead>
+		<tbody>
+			<?php 
+				$sql="select * from admin order by admin_id desc";
+				$query=$conn->query($sql);
+				while($row=$query->fetch_array()){
+					?>
+					<tr>
+						<td><?php echo date('M d, Y h:i A', strtotime($row['date']))?></td>
+						<td><?php echo $row['tblnum']; ?></td>
+						<td class="text-left">&#8369; <?php echo number_format($row['total_amount'], 2); ?></td>
+						<td><a href="#details<?php echo $row['admin_id']; ?>" data-toggle="modal" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-search"></span> View </a>
+							<?php include('sales_disp.php'); ?>
+						</td>
+					</tr>
+					<?php
+				}
+			?>
+
+		</tbody>
+	</table>
+</div>
+
+</body>
+</html>

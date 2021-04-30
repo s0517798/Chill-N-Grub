479d04a5d93c47516226d5ae5e8e68cbf846135d
PaitoneMyla <81176105+PaitoneMyla@users.noreply.github.com>
2021-05-01 02:21:29 +0800

    Add files via upload

---
+++ accountant.php
@@ -0,0 +1,17 @@
+<?php include('includes/db_conn.php'); ?>
+<?php include('heading.php') ?>
+<?php include('acc_nav.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bs/accountant.css">
+</head>
+<body>
+
+	<h1>Chill n grub</h1>
+
+
+
+</body>
+</html>

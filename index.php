0ad6521e2234ca0749a4dfb62d0041c2754bc086
FrancoBejay <81260627+rjbenedict@users.noreply.github.com>
2021-05-12 12:29:12 +0800

    update

--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
+<<<<<<< Updated upstream
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="bs/bootstrap.min.css">
+=======
+    <meta charset="utf-8">
+	<title>Chill N Grub</title>
+	<link rel="stylesheet" type="text/css" href="bs/index2.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.2-web/css/all.min.css">
+>>>>>>> Stashed changes
 </head>
 <body>
 <div class="container">
-    <div class="row mt-3">
-        <div class="col-3"></div>
-        <div class="col-6">
            <h3 class="display-2">Chill N Grub</h3>
+           <div class="textbox">
+          <i class="fa fa-user" aria-hidden="true"></i>
             <form action="includes/processlogin.php" method="post">
                 <input type="text" class="form-control my-3" name="p_username" placeholder="username">
+
+            </div>
+            <div class="textbox">
+                <i class="fa fa-key" aria-hidden="true"></i>
                 <input type="password" class="form-control my-3" name="p_password" placeholder="password">
+            </div>
                 <button class="btn btn-primary">Login</button>
             </form>
-        </div>
-        <div class="col-3"></div>
+        
     </div>
+</div>
 </div>
 
 
 
 </body>
-	<script src="bs/bootstrap.min.js"></script>
 </html>
